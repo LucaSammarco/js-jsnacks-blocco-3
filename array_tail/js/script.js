4dea48a490d6b0f5b23ba362ee81558a8c8a711c
Il tuo nome <la_tua_email@example.com>
2024-05-14 17:00:10 +0200

    trying to print last 5 numbers

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -3,8 +3,10 @@
 // Creiamo  un array chiedendo all'utente quanti elementi dovrà contenere e
 // generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire.
 // Stampiamo poi gli ultimi 5 elementi dell'Array
+
 // Bonus:
 // Chiediamo all'utente quanti elementi dell'array dovremo stampare
+
 // Suggerimenti:
 // Probabilmente  conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
 
@@ -12,8 +14,13 @@
 let userNum = Number.parseInt( prompt("Insert number"), 10);
 let userArray = []
 
+
     for (let index = 0; index < userNum; index++) {
-        userArray.push(Math.floor(Math.random()*100 +1))
+        userArray.push(Math.floor(Math.random() * 100 + 1))
         
-        console.log(userArray)
+       
     }
+
+let lastfive = userArray[userArray.lenght - 1]
+
+    console.log(lastfive)
