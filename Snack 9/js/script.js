c744066edb6bdb87c3198fee6bd3c764e1dd5a34
Il tuo nome <la_tua_email@example.com>
2024-05-15 12:29:29 +0200

    added more numbers to first array to check if it works

--- Snack 9/js/script.js
+++ Snack 9/js/script.js
@@ -2,7 +2,7 @@
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
 
-const arrayOne = [1, 2, 3, 4, 5,];
+const arrayOne = [1, 2, 3, 4, 5, 5, 3, 85, 4553, 1, 45, 453];
 const arrayTwo = [1, 2, 3, 4, 5, 6, 7];
 
 if (arrayOne.length < arrayTwo.length) {
@@ -21,4 +21,4 @@ if (arrayTwo.length < arrayOne.length) {
 
 
 
-console.log(arrayOne, arrayTwo)
+console.log(arrayOne.length, arrayTwo)
