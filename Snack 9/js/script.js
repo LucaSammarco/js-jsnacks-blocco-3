cc80a919117fe9b1febc541c386d331d89a25058
Il tuo nome <la_tua_email@example.com>
2024-05-15 12:22:43 +0200

    added if and while elemetns

--- Snack 9/js/script.js
+++ Snack 9/js/script.js
@@ -1,2 +1,13 @@
 // Crea due array che hanno un numero di elementi diversi. 
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
+
+
+const arrayOne = [1, 2, 3, 4, 5];
+const arrayTwo = [1, 2, 3, 4, 5, 6, 7];
+
+if (arrayOne.length < arrayTwo.length) {
+    while (arrayOne.length < arrayTwo.length) {
+        arrayOne.push(arrayOne.length);
+    }
+} 
+console.log(arrayOne)
