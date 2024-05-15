f3ae27ebb17a59e7ee98e64225f0a83826573672
Il tuo nome <la_tua_email@example.com>
2024-05-15 12:27:00 +0200

    added second if to check performe cicly if array two has less numbers

--- Snack 9/js/script.js
+++ Snack 9/js/script.js
@@ -2,12 +2,23 @@
 // Aggiungi elementi all'array che ha meno elementi fino a quando ne avrà tanti quanti l'altro.
 
 
-const arrayOne = [1, 2, 3, 4, 5];
+const arrayOne = [1, 2, 3, 4, 5,];
 const arrayTwo = [1, 2, 3, 4, 5, 6, 7];
 
 if (arrayOne.length < arrayTwo.length) {
     while (arrayOne.length < arrayTwo.length) {
-        arrayOne.push(arrayOne.length);
+        arrayOne.push(arrayOne.length + 1);
     }
 } 
-console.log(arrayOne)
+
+
+if (arrayTwo.length < arrayOne.length) {
+    while (arrayTwo.length < arrayOne.length) {
+        arrayTwo.push(arrayTwo.length + 1);
+    }
+} 
+
+
+
+
+console.log(arrayOne, arrayTwo)
