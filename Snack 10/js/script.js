196b154a4c9213bc444b0e01de2afa5599782869
Il tuo nome <la_tua_email@example.com>
2024-05-15 12:46:39 +0200

    looking for a solution

--- Snack 10/js/script.js
+++ Snack 10/js/script.js
@@ -2,3 +2,15 @@
 // Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
 
 const userArray = []
+
+let sum = 0
+
+
+let userNumber = Number.parseInt( prompt("Insert number"), 10);
+
+while (sum > 50) {
+
+    userArray.push(userNumber);
+    userNumber += sum;
+    
+}
