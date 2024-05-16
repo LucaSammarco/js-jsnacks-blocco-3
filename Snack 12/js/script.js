f9e5c9f0846ac0d0fe4ff9f7115db2f9324aaec7
Il tuo nome <la_tua_email@example.com>
2024-05-16 11:32:02 +0200

    thinking ***

--- Snack 12/js/script.js
+++ Snack 12/js/script.js
@@ -1,2 +1,17 @@
 
-// Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+// TODO Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+
+
+// let firstWord = prompt("First word");
+
+// let secondWord = prompt("Second word");
+
+
+function twoWords(firstWord, secondWord) {
+
+    if (firstWord.lenght === secondWord.lenght) {
+        
+    }
+    
+}
