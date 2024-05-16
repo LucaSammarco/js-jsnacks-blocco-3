541925edb767b9a5f621cde1a1039604cac3d1f5
Il tuo nome <la_tua_email@example.com>
2024-05-16 12:21:52 +0200

    function works but I had to look for the invert number expression

---
+++ Snack 13/js/script.js
@@ -0,0 +1,14 @@
+// > Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
+
+
+function getReverse(numero) {
+
+    let numeroStringa = numero.toString();
+    let invertito = numeroStringa.split("").reverse().join("");
+
+    let numeroInvertito = parseInt(invertito, 10)
+
+    return numeroInvertito;
+  }
+
+ 
