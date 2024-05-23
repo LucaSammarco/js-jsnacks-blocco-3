44f2fe8ca887cb2a5fc1dd92c5315022e6474558
Il tuo nome <la_tua_email@example.com>
2024-05-23 12:10:43 +0200

    added automobili

--- Snack 15/js/script.js
+++ Snack 15/js/script.js
@@ -2,3 +2,24 @@
 // > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
 // < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
 // < Infine stampa separatamente i 3 array.
+
+
+const automobili = [
+  { marca: 'Toyota', modello: 'berlina', alimentazione: 'benzina' },
+  { marca: 'Honda', modello: 'berlina', alimentazione: 'diesel' },
+  { marca: 'Ford', modello: 'cabriolet', alimentazione: 'benzina' },
+  { marca: 'Chevrolet', modello: 'cabriolet', alimentazione: 'benzina' },
+  { marca: 'BMW', modello: 'roadster', alimentazione: 'benzina' },
+  { marca: 'Audi', modello: 'berlina', alimentazione: 'diesel' },
+  { marca: 'Mercedes-Benz', modello: 'berlina', alimentazione: 'elettrico' },
+  { marca: 'Volkswagen', modello: 'station wagon', alimentazione: 'diesel' },
+  { marca: 'Hyundai', modello: 'SUV', alimentazione: 'gpl' },
+  { marca: 'Nissan', modello: 'SUV', alimentazione: 'metano' },
+  { marca: 'Kia', modello: 'crossover', alimentazione: 'elettrico' },
+  { marca: 'Subaru', modello: 'berlina', alimentazione: 'benzina' },
+  { marca: 'Mazda', modello: 'berlina', alimentazione: 'diesel' },
+  { marca: 'Tesla', modello: 'berlina', alimentazione: 'elettrico' },
+  { marca: 'Volvo', modello: 'station wagon', alimentazione: 'metano' }
+];
+
+
