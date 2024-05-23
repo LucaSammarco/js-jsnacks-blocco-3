34a37d5834c56280daea98db4e2c645e408a3938
Il tuo nome <la_tua_email@example.com>
2024-05-23 12:04:19 +0200

    set up

--- Snack 15/js/script.js
+++ Snack 15/js/script.js
@@ -1,16 +1,4 @@
-// Creare un oggetto palla che abbia le seguenti proprietà.
-// Nome = palla
-// Peso = 10
-// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
-
-
-const palla = {
-  nome: "Palla",
-  peso: 10
-}
-console.log(palla)
-let nuovoPeso = parseInt(prompt("inserisci peso"));
-
-palla.peso = nuovoPeso;
-
-console.log(palla)
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
---
+++ Snack objects-2/js/script.js
@@ -0,0 +1,16 @@
+// Creare un oggetto palla che abbia le seguenti proprietà.
+// Nome = palla
+// Peso = 10
+// Attraverso un prompt dare la possibilità all'utente di modificare il peso della palla.
+
+
+const palla = {
+  nome: "Palla",
+  peso: 10
+}
+console.log(palla)
+let nuovoPeso = parseInt(prompt("Inserisci nuovo peso"));
+
+palla.peso = nuovoPeso;
+
+console.log(palla)
